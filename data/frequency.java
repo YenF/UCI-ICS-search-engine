179971c92a3934067007ea06c82da0bf4a47e021
FrankZhang <zyf.bupt@gmail.com>
2016-01-26 22:08:39 -0800

    helpers

---
+++ data/frequency.java
@@ -0,0 +1,13 @@
+package data;
+
+/**
+ * data整个这个文件夹交给建霖了
+ * 计算frequency在这里
+ * 另外还需要计算3-grams及其frequency
+ *
+ * data的输入源从DB那边抓
+ * 抓取输入源的代码可以由雁丰写
+ */
+public final class frequency {
+
+}
--- helpers/StopWord.java
+++ helpers/StopWord.java
@@ -6,10 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
-import java.util.ArrayList;
 
 public class StopWord {
 
