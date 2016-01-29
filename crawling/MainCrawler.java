071bb82a6a6e99fef4cfe6f5bcd89dc08d5cdea3
FrankZhang <zyf.bupt@gmail.com>
2016-01-28 17:39:20 -0800

    main function
    crawl the uci

--- crawling/MainCrawler.java
+++ crawling/MainCrawler.java
@@ -2,9 +2,18 @@ package crawling;
 
 import storage.docStore;
 
+import java.util.Collection;
+
 public class MainCrawler {
 	public static void main(String[] args) {
-
 		docStore docStore = new docStore("storage");
+		try {
+			Collection<String> crawledUrls = crawler.crawl("http://www.ics.uci.edu", docStore);
+			System.out.println("Finished crawling " + crawledUrls.size() + " page(s)");
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			docStore.close();
+		}
 	}
 }
