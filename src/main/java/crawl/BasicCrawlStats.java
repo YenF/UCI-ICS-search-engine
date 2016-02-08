dd4524932d996e6e73686432553b715b49173044
FrankZhang <zyf.bupt@gmail.com>
2016-02-08 10:38:15 -0800

    remove helper

--- src/main/java/crawl/BasicCrawlStats.java
+++ src/main/java/crawl/BasicCrawlStats.java
@@ -14,9 +14,16 @@ public class BasicCrawlStats {
 
     public boolean intendToVisit(String url) throws URISyntaxException {
 
-        String urlR = URLhelper.removeQuery(url);
-        if (urlR == null)
-            return true;
+        String urlR = url.substring(0, url.lastIndexOf("?"));
+        String str = "calendar";
+
+        if (urlR.toLowerCase().contains(str)){
+            return false;
+        }
+
+        /**
+         * if (urlR == null) { return true; }
+         */
 
         int count = 1;
         if (this.pagesToCrawl.containsKey(urlR))
